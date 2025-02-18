73e52a5597ef465e6dc15a67dabeac96ace1dacf
Wioletta Koperska <koperskawioletta@gmail.com>
2025-02-18 17:28:45 +0100

    add currencies

---
+++ src/App/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+  {
+    short: "EUR",
+    name: "Euro",
+    rate: 4.38,
+  },
+  {
+    short: "USD",
+    name: "Dolar Amerykański",
+    rate: 3.93,
+  },
+  {
+    short: "GBP",
+    name: "Funt Brytyjski",
+    rate: 4.94,
+  },
+];
