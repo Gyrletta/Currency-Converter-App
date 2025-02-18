597149b08ed0bc8487b015b5278b1e301aceb13b
Wioletta Koperska <koperskawioletta@gmail.com>
2025-02-18 21:01:09 +0100

    changes in App.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { currencies } from "./currencies";
+import Form from "./Form/Form";
 import "./App.css";
 
 function App() {
@@ -8,7 +9,9 @@ function App() {
   const [result, setResult] = useState(null);
 
   const calculateResult = (amount, currency) => {
-    const currencyData = currencies.find((c) => c.short === currency);
+    const currencyData = currencies.find(
+      (currencyItem) => currencyItem.short === currency
+    );
     if (!currencyData) return 0;
 
     return amount / currencyData.rate;
@@ -28,53 +31,23 @@ function App() {
 
   return (
     <div className="container">
-      <fieldset className="field">
-        <form className="form" onSubmit={formSubmit}>
-          <p>
-            <h1 className="header">Currency Convert</h1>
-            <label className="form__text">Kwota(PLN)*</label>
-            <input
-              className="form__input amount"
-              type="number"
-              value={amount}
-              onChange={({ target }) => setAmount(target.value)}
-              autofocus
-              placeholder="Wprowadź kwotę:"
-              required
-              step="0.01"
-              min="0.01"
-            />
-          </p>
-          <p>
-            <label className="form__text">
-              <span className="form__labelText">Waluta:</span>
-              <select
-                className="form__field currency"
-                value={currency}
-                onChange={({ target }) => setCurrency(target.value)}
-              >
-                {currencies.map((currency) => (
-                  <option key={currency.short} value={currency.short}>
-                    {currency.name}
-                  </option>
-                ))}
-              </select>
-            </label>
-          </p>
-          <p>
-            <button type="submit" className="form__button">
-              Przelicz!
-            </button>
-          </p>
-        </form>
-      </fieldset>
+      <Form
+        amount={amount}
+        setAmount={setAmount}
+        currency={currency}
+        setCurrency={setCurrency}
+        formSubmit={formSubmit}
+        currencies={currencies}
+      />
 
       {result !== null && (
         <p className="result">
-          {amount} PLN ={" "}
           <strong>
-            {result.toFixed(2)}{" "}
-            {currencies.find((c) => c.short === currency)?.name}
+            {amount} PLN = {result.toFixed(2)}{" "}
+            {
+              currencies.find((currencyItem) => currencyItem.short === currency)
+                ?.name
+            }
           </strong>
         </p>
       )}
