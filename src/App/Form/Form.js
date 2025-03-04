dd32f421108a83cd5de232b7052cf8c3aba4567c
Wioletta Koperska <koperskawioletta@gmail.com>
2025-03-04 18:54:54 +0100

    create styled.js for Form

--- src/App/Form/Form.js
+++ src/App/Form/Form.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Result } from "../Form/Result/Result";
-import "./Form.css";
 import { currencies } from "../currencies";
+import { Button, Field, Header, Info, LabelText } from "./styled";
 
 const Form = ({ calculateResult, result }) => {
   const [currency, setCurrency] = useState(currencies[0].short);
@@ -18,44 +18,42 @@ const Form = ({ calculateResult, result }) => {
   };
 
   return (
-    <form className="form" onSubmit={onSubmit}>
-      <h1 className="header">Currency Converter</h1>
+    <form onSubmit={onSubmit}>
+      <Header>Currency Converter</Header>
       <p>
-        <label className="form__text">Kwota (PLN)*</label>
-        <input
-          className="form__input amount"
-          type="number"
-          value={amount}
-          onChange={({ target }) => setAmount(target.value)}
-          autoFocus
-          placeholder="Wprowadź kwotę"
-          required
-          step="0.01"
-          min="0.01"
-        />
+        <label>
+          <LabelText>Kwota (PLN)*</LabelText>
+          <Field
+            value={amount}
+            onChange={({ target }) => setAmount(target.value)}
+            autoFocus
+            placeholder="Wprowadź kwotę"
+            required
+            step="0.01"
+            min="0.01"
+          />
+        </label>
       </p>
       <p>
-        <label className="form__text">
-          <span className="form__labelText">Waluta:</span>
-          <select
-            className="form__field currency"
+        <label>
+          <LabelText>Waluta:</LabelText>
+          <Field
+            as="select"
             value={currency}
             onChange={({ target }) => setCurrency(target.value)}
           >
             {currencies.map(({ short, name }) => (
               <option key={short} value={short}>
-                {name}
+                {currency.name}
               </option>
             ))}
-          </select>
+          </Field>
         </label>
       </p>
       <p>
-        <button type="submit" className="form__button">
-          Przelicz!
-        </button>
+        <Button>Przelicz!</Button>
       </p>
-      <p>Według średniego kursu NBP z dn. 17.10.23</p>
+      <Info>Według średniego kursu NBP z dn. 17.10.23</Info>
       <Result result={result} />
     </form>
   );
