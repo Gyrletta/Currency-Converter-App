749dd820e16184eceb85eeedf4b115192beac142
Wioletta Koperska <koperskawioletta@gmail.com>
2025-03-04 18:54:14 +0100

    create styled.js for Clock

---
+++ src/App/Clock/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  text-align: right;
+  font-family: Arial, Helvetica, sans-serif;
+  color: ${({ theme }) => theme.colors.white};
+`;
